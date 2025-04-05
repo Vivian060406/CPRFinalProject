5cc7efd5a42d53295f18afb44c5a18d2d66d3924
Vivian060406 <khahvy2006@gmail.com>
2025-04-05 12:20:29 -0400

    Add manipulating.c

--- manipulating.c
+++ manipulating.c
@@ -3,6 +3,24 @@
 #include "manipulating.h"
 
 void manipulating() {
+    /* Version 1 */
+    printf("*** Start of Concatenating Strings Demo ***\n");
+    char string1[BUFFER_SIZE];
+    char string2[BUFFER_SIZE];
+    do {
+        printf("Type the 1st string (q - to quit):\n");
+        fgets(string1, BUFFER_SIZE, stdin);
+        string1[strlen(string1) - 1] = '\0';
+        if (strcmp(string1, "q") != 0) {
+            printf("Type the 2nd string: \n");
+            fgets(string2, BUFFER_SIZE, stdin);
+            string2[strlen(string2) - 1] = '\0';
+            strcat(string1, string2);
+            printf("Concatenated string is \"%s\"\n", string1);
+        }
+    } while (strcmp(string1, "q") != 0);
+    printf("*** End of Concatenating Strings Demo ***\n\n");
+
     /* Version 2 */
     printf("*** Start of Comparing Strings Demo ***\n");
     char compare1[BUFFER_SIZE];
